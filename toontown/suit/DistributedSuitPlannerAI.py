2b3e211e531b535b0abd6b6dffb6c13800aea2f6
dakotachasesmith <dakotachasesmith@gmail.com>
2020-06-17 09:41:32 -1000

    Fix bug when spawning lower level cogs for invasions

--- toontown/suit/DistributedSuitPlannerAI.py
+++ toontown/suit/DistributedSuitPlannerAI.py
@@ -1416,9 +1416,7 @@ class DistributedSuitPlannerAI(DistributedObjectAI.DistributedObjectAI, SuitPlan
             typeChoices = xrange(max(level - 4, 1), min(level, self.MAX_SUIT_TYPES) + 1)
             type = random.choice(typeChoices)
 
-        level = max(level, type)
-        if level not in ToontownGlobals.SuitLevels:
-            level = min(max(level, type), type + 4)
+        level = min(max(level, type), type + 4)
 
         if track == None:
             track = SuitDNA.suitDepts[SuitBattleGlobals.pickFromFreqList(self.SuitHoodInfo[self.hoodInfoIdx]["tracks"])]
